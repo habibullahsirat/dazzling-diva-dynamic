1b7f4bf4e3288577eaccd3167bbb17d5a6ef41c3
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-08 18:33:37 +0600

    Hero route updated.

---
+++ dazzling-diva-admin/app/admin/hero/page.jsx
@@ -0,0 +1,13 @@
+import HeroForm from "@/components/Hero/HeroForm";
+import HeroList from "@/components/Hero/HeroList";
+import HeroSection from "@/components/Hero/HeroSection";
+
+export default function Hero() {
+  return (
+    <>
+      {/* <HeroList />
+      <HeroForm /> */}
+      <HeroSection />
+    </>
+  );
+}
