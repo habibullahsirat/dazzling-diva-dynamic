3925f81ad86beeb1c6f3c89f29dc82653c291873
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-13 01:45:27 +0600

    Code updated.

--- dazzling-diva-admin/components/LoadingSpinner.jsx
+++ dazzling-diva-admin/components/LoadingSpinner.jsx
@@ -1,7 +1,7 @@
 export default function LoadingSpinner() {
   return (
     <div className="flex justify-center py-16">
-      <div className="h-10 w-10 animate-spin rounded-full border-4 border-blue-600 border-t-transparent"></div>
+      <div className="h-10 w-10 animate-spin rounded-full border-4 border-[#5A0C3D] border-t-transparent"></div>
     </div>
   );
 }
