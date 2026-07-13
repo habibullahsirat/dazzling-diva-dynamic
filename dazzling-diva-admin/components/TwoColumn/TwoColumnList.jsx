600b89f600f2178048251fd153d83f4a2c1f741c
Habibulah Sirat <63021173+habibullahsirat@users.noreply.github.com>
2026-07-14 01:55:45 +0600

    Cloudinary badge updated.

--- dazzling-diva-admin/components/TwoColumn/TwoColumnList.jsx
+++ dazzling-diva-admin/components/TwoColumn/TwoColumnList.jsx
@@ -48,6 +48,11 @@ export default function TwoColumnList({ items, fetchItems, onEdit }) {
                   fill
                   className="rounded object-cover object-top"
                 />
+                {item.left.image?.includes("cloudinary") && (
+                  <div className="absolute top-2 right-2 bg-gradient-to-r from-[#4A0932] via-[#5A0C3D] to-[#8A0B5B] text-white text-xs px-2 py-1 rounded-full">
+                    Cloudinary
+                  </div>
+                )}
               </div>
 
               <h3 className="mt-3 font-bold">{item.left.categoryName}</h3>
@@ -67,6 +72,11 @@ export default function TwoColumnList({ items, fetchItems, onEdit }) {
                   fill
                   className="rounded object-cover object-top"
                 />
+                {item.right.image?.includes("cloudinary") && (
+                  <div className="absolute top-2 right-2 bg-gradient-to-r from-[#4A0932] via-[#5A0C3D] to-[#8A0B5B] text-white text-xs px-2 py-1 rounded-full">
+                    Cloudinary
+                  </div>
+                )}
               </div>
 
               <h3 className="mt-3 font-bold">{item.right.categoryName}</h3>
